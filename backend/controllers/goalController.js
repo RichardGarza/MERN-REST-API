f4018d8d3cd4850328bda836e9f7cea0b6e0c9e7
Richard Garza <44719153+RichardGarza@users.noreply.github.com>
2025-11-12 13:39:25 -0800

    Changed CRUD functions to async to plan for database, setup mongoDB, initiated connection in server.js

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,29 +1,37 @@
-const getGoals = (req, res) => {res.status(200).json({ message: 'All Goals' });}
 
-const getGoal = (req, res) =>{
+// Express built in async handler to handle catch / errors
+const asyncHandler = require('express-async-handler')
+
+// Get All Goals
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'All Goals' });
+})
+
+// Get Single Goal
+const getGoal = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Goal retrieved, goal id: ${req.params.id}` });
-}
+})
 
 // Create Goal with error handling
-const createGoal = (req, res) => {
+const createGoal = asyncHandler(async (req, res) => {
    if (!req.body) {
     res.status(400)
     throw new Error('Please add a text field')
   } 
-  
     res.status(200).json({ message: `This is your text: ${req.body.text}` })
-  
-}
+})
 
-const updateGoal = (req, res) =>{
+// Update Goal
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Updated goal id: ${req.params.id}` });
-}
+})
 
-const deleteGoal = (req, res) => {
+// Delete Goal
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Deleted goal id: ${req.params.id}` });
-}
+})
 
 module.exports = {
   getGoals,
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const {errorHandler} = require('./middleware/errorMiddleware')
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+connectDB()
+
 const app = express();
+
 
 // Enable express to accept / parse urlencoded & json
 app.use(express.json());
